82b1dc45eaee984eb71ec848a34aab3b70f78aec
luis92arma <luis.arma92@gmail.com>
2016-07-22 11:19:01 -0500

    views de app twixter

--- twiterapp/twixter/views.py
+++ twiterapp/twixter/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.models import User
+from .models import Twixt
+from .forms import *
 
-# Create your views here.
+class ListViewTwix(View):
+    """docstring for """
+    def get(self, arg):
+        template_name = 'base.html'
+        #user = User.object.get()
+        twixs = user.blog_posts.all()
+        context = {
+            'twits': twixs,
+        }
+        return render(request, template_name, context)
+
+class DetailViewTiwx(View):
+    """docstring for """
+    def get(self, arg, slug):
+        template_name = 'detalleTwix.html'
+        post = Twixt.object.get(slug = slug)
+        context = {
+            'twits': post,
+        }
+        return render(request, template_name, context)
+
+class NewTwit(View):
+    """docstring for NewTwit"""
+    def get(self, arg):
+        template_name = 'NewTwixter.html'
+        form = PostForm()
+        context = {
+            'form': form
+        }
+        return render(request, template_name, context)
